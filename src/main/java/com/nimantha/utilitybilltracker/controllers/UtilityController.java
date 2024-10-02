2e625d8d1e0a3e2418f9d1346e816b10b9310787
Nimantha Cooray <nimanthacooray@gmail.com>
2024-10-02 21:29:35 +0530

    Code cleanup

--- src/main/java/com/nimantha/utilitybilltracker/controllers/UtilityController.java
+++ src/main/java/com/nimantha/utilitybilltracker/controllers/UtilityController.java
@@ -4,8 +4,6 @@ import com.nimantha.utilitybilltracker.dto.*;
 import com.nimantha.utilitybilltracker.services.UtilityService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UtilityController {
     private final UtilityService utilityService;
-    private final Logger logger = LoggerFactory.getLogger(UtilityController.class);
 
     @PostMapping
     public CreateUtilityResponse createUtility(@Valid @RequestBody CreateUtilityRequest createUtilityRequest) {
@@ -36,6 +33,6 @@ public class UtilityController {
     @GetMapping
     public CustomPageDTO<UtilityDTO> getAllUtilities(@RequestParam(defaultValue = "0") int page,
                                                      @RequestParam(defaultValue = "10") int size) {
-        return new CustomPageDTO<UtilityDTO>(utilityService.getUtilities(page, size));
+        return new CustomPageDTO<>(utilityService.getUtilities(page, size));
     }
 }
