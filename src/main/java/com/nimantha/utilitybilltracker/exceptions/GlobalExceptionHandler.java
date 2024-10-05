40f70777f66d3d63f9475c9a375ed84418a84ac1
Nimantha Cooray <nimanthacooray@gmail.com>
2024-10-05 20:31:50 +0530

    Add an exception handler for MethodArgumentNotValidException

--- src/main/java/com/nimantha/utilitybilltracker/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/nimantha/utilitybilltracker/exceptions/GlobalExceptionHandler.java
@@ -4,9 +4,13 @@ import com.nimantha.utilitybilltracker.dto.ErrorResponse;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,5 +26,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleEntityNotFound(UserAlreadyExistsException ex) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+          .getFieldErrors()
+          .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
     }
 }
